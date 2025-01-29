e348af902e24c589270a235b155d4324a505e189
sib-vishal <vishal.maurya@sibinfo.tech>
2025-01-29 09:41:40 +0530

    initial commit

---
+++ app/Views/index.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/layout.php') ?>
+
+<?= $this->section('title') ?>
+Admin Dashboard Panel
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+<!-- Your dashboard content goes here -->
+<div>
+    <h1>Welcome to the Dashboard</h1>
+</div>
+<?= $this->endSection() ?>
